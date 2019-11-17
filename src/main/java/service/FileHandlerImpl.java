a2c33458bb1e714d6e3d91675e7e2bffddae7264
Igor Kolesnikov <mexko@yandex.ru>
2019-11-17 13:50:01 +0300

    * improve file log parser

--- src/main/java/service/FileHandlerImpl.java
+++ src/main/java/service/FileHandlerImpl.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
 public class FileHandlerImpl implements FileHandler {
